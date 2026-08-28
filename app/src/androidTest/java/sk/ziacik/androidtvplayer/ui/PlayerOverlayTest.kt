6e929c73b5ef6aa27cc1de3533945efba285651c
František Žiačik <ziacik@gmail.com>
2026-08-28 22:10:41 +0200

    test: fix Compose assertion import

--- app/src/androidTest/java/sk/ziacik/androidtvplayer/ui/PlayerOverlayTest.kt
+++ app/src/androidTest/java/sk/ziacik/androidtvplayer/ui/PlayerOverlayTest.kt
@@ -1,6 +1,5 @@
 package sk.ziacik.androidtvplayer.ui
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
