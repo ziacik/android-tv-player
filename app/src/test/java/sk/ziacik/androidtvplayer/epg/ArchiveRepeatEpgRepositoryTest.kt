6267357c36311ef0c33635bc3d0ac15583039c83
František Žiačik <ziacik@gmail.com>
2026-09-22 20:09:43 +0200

    Test archive repeat across differing EPG titles

--- app/src/test/java/sk/ziacik/androidtvplayer/epg/ArchiveRepeatEpgRepositoryTest.kt
+++ app/src/test/java/sk/ziacik/androidtvplayer/epg/ArchiveRepeatEpgRepositoryTest.kt
@@ -25,6 +25,40 @@ class ArchiveRepeatEpgRepositoryTest {
     }
 
     @Test
+    fun `links repeat through secondary EPG even when primary title differs`() = runTest {
+        val repository = CachedXmltvEpgRepository(
+            sources = listOf(
+                XmltvEpgSource(
+                    id = EpgSourceId.OPEN_EPG,
+                    cacheFile = File(temporaryFolder.root, "open-doctor.xml"),
+                    download = {
+                        """<tv>
+                            <programme channel="Jednotka HD.sk" start="20260922020500 +0200" stop="20260922025000 +0200"><title>Doktor z hôr - Chladné ticho</title></programme>
+                        </tv>""".trimIndent().encodeToByteArray()
+                    },
+                ),
+                XmltvEpgSource(
+                    id = EpgSourceId.SKYLINK,
+                    cacheFile = File(temporaryFolder.root, "skylink-doctor.xml"),
+                    download = {
+                        """<tv>
+                            <programme channel="jednotka-skylink" start="20260921145500 +0200" stop="20260921154000 +0200"><title>Doktor z hôr: Nové príbehy XV (2)</title><sub-title>Chladné ticho - 2. časť.</sub-title></programme>
+                            <programme channel="jednotka-skylink" start="20260922020500 +0200" stop="20260922025000 +0200"><title>Doktor z hôr: Nové príbehy XV (2)</title><sub-title>Chladné ticho - 2. časť.</sub-title></programme>
+                        </tv>""".trimIndent().encodeToByteArray()
+                    },
+                ),
+            ),
+            clockMs = { DOCTOR_REPEAT_MS },
+            parser = XmltvEpgParser(),
+        )
+
+        val programme = repository.currentProgram(TEST_CHANNEL, DOCTOR_REPEAT_MS)
+
+        assertEquals("Doktor z hôr - Chladné ticho", programme?.title)
+        assertEquals(DOCTOR_ORIGINAL_MS, programme?.archiveOriginalStartsAtMs)
+    }
+
+    @Test
     fun `does not mark a new numbered episode as an archive repeat`() = runTest {
         val repository = repository()
 
@@ -66,6 +100,8 @@ class ArchiveRepeatEpgRepositoryTest {
         const val PREVIOUS_EVENING_DUEL_MS = 1_788_882_300_000L
         const val MORNING_DUEL_MS = 1_788_943_500_000L
         const val EVENING_DUEL_MS = 1_788_968_700_000L
+        const val DOCTOR_ORIGINAL_MS = 1_789_995_300_000L
+        const val DOCTOR_REPEAT_MS = 1_790_035_500_000L
         val TEST_CHANNEL = TvChannel.JEDNOTKA.copy(
             epgIds = mapOf(
                 EpgSourceId.OPEN_EPG to "Jednotka HD.sk",
